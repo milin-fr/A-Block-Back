41e8530425426c4fd6c17fa5b03fc3133d2f8b59
unknown <m.ilin.fr@gmail.com>
2020-03-04 21:00:39 +0100

    list of api accesses updated on home page

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -13,47 +13,175 @@ class MainController extends AbstractController
     public function home()
     {
         $apiData = [
-            "api/access_level" => [
-                "GET" => "les access_levels"
+            [
+                "api/access_level",
+                "GET",
+                "list access_levels",
+                ""
             ],
-            "api/access_level/id" => [
-                "GET" => "l'access_level par id"
+            [
+                "api/access_level",
+                "POST",
+                "add access_level",
+                "
+                {
+                    \"title\": \"string, not blank\",
+                }
+                "
             ],
-            "api/exercise" => [
-                "GET" => "les exercises"
+            [
+                "api/access_level/id",
+                "GET",
+                "access_level by id",
+                ""
             ],
-            "api/exercise/id" => [
-                "GET" => "l'exercise par id"
+            [
+                "api/exercise",
+                "GET",
+                "list exercises",
+                ""
             ],
-            "api/hint" => [
-                "GET" => "les hints"
+            [
+                "api/exercise",
+                "POST",
+                "add exercise",
+                "
+                {
+                    \"title\": \"string, not blank\",
+                    \"time\": integer,
+                    \"imgPath\": \"string\",
+                    \"description\": \"string, not blank\",
+                    \"score\": integer,
+                    \"hints\": [array of integers],
+                    \"prerequisites\": [array of integers],
+                    \"programs\": [array of integers]
+                }
+                "
             ],
-            "api/hint/id" => [
-                "GET" => "le hint par id"
+            [
+                "api/exercise/id",
+                "GET",
+                "exercise by id",
+                ""
             ],
-            "api/mastery_level" => [
-                "GET" => "les mastery_levels"
+            [
+                "api/hint",
+                "GET",
+                "list hints",
+                ""
             ],
-            "api/mastery_level/id" => [
-                "GET" => "le mastery_level par id"
+            [
+                "api/hint",
+                "POST",
+                "add hint",
+                "
+                {
+                    \"text\": \"string, not blank\",
+                }
+                "
             ],
-            "api/prerequisite" => [
-                "GET" => "les prerequisites"
+            [
+                "api/hint/id",
+                "GET",
+                "hint by id",
+                ""
             ],
-            "api/prerequisite/id" => [
-                "GET" => "le prerequisite par id"
+            [
+                "api/mastery_level",
+                "GET",
+                "list mastery_levels",
+                ""
             ],
-            "api/program" => [
-                "GET" => "les programs"
+            [
+                "api/mastery_level",
+                "POST",
+                "add mastery_level",
+                "
+                {
+                    \"title\": \"string, not blank\",
+                }
+                "
             ],
-            "api/program/id" => [
-                "GET" => "le program par id"
+            [
+                "api/mastery_level/id",
+                "GET",
+                "mastery_level by id",
+                ""
             ],
-            "api/user" => [
-                "GET" => "les users"
+            [
+                "api/prerequisite",
+                "GET",
+                "list prerequisites",
+                ""
             ],
-            "api/user/id" => [
-                "GET" => "l'user par id"
+            [
+                "api/prerequisite",
+                "POST",
+                "add prerequisite",
+                "
+                {
+                    \"description\": \"string, not blank\",
+                }
+                "
+            ],
+            [
+                "api/prerequisite/id",
+                "GET",
+                "prerequisite by id",
+                ""
+            ],
+            [
+                "api/program",
+                "GET",
+                "list programs",
+                ""
+            ],
+            [
+                "api/program",
+                "POST",
+                "add program",
+                "
+                {
+                    \"title\": \"string, not blank\",
+                    \"time\": integer,
+                    \"imgPath\": \"string\",
+                    \"description\": \"string, not blank\",
+                    \"exercises\":[array of integers]
+                }
+                "
+            ],
+            [
+                "api/program/id",
+                "GET",
+                "program by id",
+                ""
+            ],
+            [
+                "api/user",
+                "GET",
+                "list users",
+                ""
+            ],
+            [
+                "api/user",
+                "POST",
+                "add user",
+                "
+                {
+                    \"email\": \"email, not blank\",
+                    \"password\": \"string, not blank\",
+                    \"accountName\": \"string, not blank\",
+                    \"imgPath\": \"string\",
+                    \"availableTime\": integer,
+                    \"masteryLevel\": integer
+                }
+                "
+            ],
+            [
+                "api/user/id",
+                "GET",
+                "user by id",
+                ""
             ],
         ];
 
