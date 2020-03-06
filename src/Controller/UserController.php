7508f5a143f1949f152d48f3b2e2de8936d7309b
unknown <m.ilin.fr@gmail.com>
2020-03-06 12:03:56 +0100

    user put

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -190,21 +190,148 @@ class UserController extends AbstractController
     /**
      * @Route("/{id}", name="user_edit", methods={"PUT"})
      */
-    public function putAblocUser(Request $request, User $user): Response
+    public function putAblocUser(Request $request, $id, UserRepository $userRepository, AccessLevelRepository $accessLevelRepository, MasteryLevelRepository $masteryLevelRepository): Response
     {
-        $form = $this->createForm(UserType::class, $user);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $this->getDoctrine()->getManager()->flush();
-
-            return $this->redirectToRoute('user_index');
+        /*
+            {
+                "email": "user@mail.com",
+                "password": 1234,
+                "account_name": "top velu",
+                "img_path": "image_profile_1.png",
+                "available_time": 10,
+                "mastery_level": 1
+            }
+        */
+        
+        $ablocUser = $userRepository->find($id);
+        if (!$ablocUser) {
+            
+            return new JsonResponse(['error' => '404 not found.'], 404);
         }
 
-        return $this->render('user/edit.html.twig', [
-            'user' => $user,
-            'form' => $form->createView(),
-        ]);
+        // start of payload validation
+        $keyList = ["email", "password", "account_name", "img_path", "available_time", "mastery_level"];
+
+        $validationsErrors = [];
+
+        $jsonContent = $request->getContent();
+        if (json_decode($jsonContent) === null) {
+            return $this->json([
+                'error' => 'Format de données érroné.'
+            ], Response::HTTP_UNPROCESSABLE_ENTITY);
+        }
+
+        // get payload content and convert it to object, so we can acess it's properties
+        $contentObject = json_decode($request->getContent());
+        $contentArray = get_object_vars($contentObject);
+
+        foreach($keyList as $key){
+            if(!array_key_exists($key, $contentArray)){
+                $validationsErrors[] = [
+                                        $key => "Requiered, but not provided"
+                                        ];
+            }
+        }
+
+        if (count($validationsErrors) !== 0) {
+            return $this->json($validationsErrors, Response::HTTP_UNPROCESSABLE_ENTITY);
+        }
+        // end of payload validation
+
+        // get payload content and convert it to object, so we can acess it's properties
+
+        // values validation
+
+        $userEmail = $contentObject->email;
+        $userPassword = $contentObject->password;
+        $userAccountName = $contentObject->account_name;
+        $userImgPath = $contentObject->img_path;
+        $userAvailableTime = $contentObject->available_time;
+        $userMasteryLevel = $contentObject->mastery_level;
+
+
+        $validationsErrors = [];
+        
+        if(!filter_var($userEmail, FILTER_VALIDATE_EMAIL)){
+            $validationsErrors[] = "email, notvalid";
+        }
+
+        $users = $userRepository->findAll();
+        $emails = [];
+        foreach($users as $user){
+            $emails[] = $user->getEmail();
+        }
+
+        if(!($ablocUser->getEmail() === $userEmail)){
+            if(in_array($userEmail, $emails)){
+                $validationsErrors[] = "email, exists"; // peut se faire avec validateur * @Assert\Unique
+            }
+        }
+
+        if(strlen($userPassword) > 32){
+            $validationsErrors[] = "password, length, max, 32";
+        }
+
+        if(strlen($userPassword) < 3){
+            $validationsErrors[] = "password, length, min, 3";
+        }
+
+        if(strlen($userAccountName) > 64){
+            $validationsErrors[] = "accountName, length, max, 32";
+        }
+
+        if(strlen($userAccountName) < 3){
+            $validationsErrors[] = "accountName, length, min, 3";
+        }
+
+        if(strlen($userImgPath) > 64){
+            $validationsErrors[] = "imgPath, length, max, 64";
+        }
+
+        if($userAvailableTime < 0){
+            $validationsErrors[] = "availableTime, value, min, 0";
+        }
+
+        if($userAvailableTime > 999){
+            $validationsErrors[] = "availableTime, value, max, 999";
+        }
+
+        if(gettype($userMasteryLevel) !== "integer"){
+            $validationsErrors[] = "masteryLevel, not integer";
+        }
+
+        if(gettype($userMasteryLevel) === "integer"){
+            $masteryLevel = $masteryLevelRepository->find($userMasteryLevel);
+            if(!$masteryLevel){
+                $validationsErrors[] = "masteryLevel, no match in bdd";
+            }
+        }
+
+        if (count($validationsErrors) !== 0) {
+            return $this->json($validationsErrors, Response::HTTP_UNPROCESSABLE_ENTITY);
+        }
+
+
+        $user->setEmail($userEmail);
+        $user->setPassword($userPassword); // a revoir
+        $user->setAccountName($userAccountName);
+        if($userImgPath === ""){
+            $userImgPath = "default_user.png";
+        }
+        $user->setImgPath($userImgPath);
+        $user->setAvailableTime(($userAvailableTime));
+        $user->setScore(0);
+        $user->setUpdatedAt(new \DateTime());
+        
+        $accesLevel = $accessLevelRepository->findAll();
+        $user->setAccessLevel($accesLevel[0]); // a revoir
+        
+        $user->setMasteryLevel($masteryLevel);
+        
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($user);
+        $em->flush();
+        return $this->redirectToRoute('user_show', ['id' => $user->getId()], Response::HTTP_CREATED);
     }
 
     /**
