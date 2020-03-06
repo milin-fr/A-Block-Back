a6a1e3928309cb99a0e5555ee664f290bb8369e0
unknown <m.ilin.fr@gmail.com>
2020-03-06 11:42:09 +0100

    mastery level put

--- src/Controller/MasteryLevelController.php
+++ src/Controller/MasteryLevelController.php
@@ -137,21 +137,97 @@ class MasteryLevelController extends AbstractController
     /**
      * @Route("/{id}", name="mastery_level_edit", methods={"PUT"})
      */
-    public function putMasterLevel(Request $request, MasteryLevel $masteryLevel): Response
+    public function putMasterLevel(Request $request, $id, MasteryLevelRepository $masteryLevelRepository): Response
     {
-        $form = $this->createForm(MasteryLevelType::class, $masteryLevel);
-        $form->handleRequest($request);
 
-        if ($form->isSubmitted() && $form->isValid()) {
-            $this->getDoctrine()->getManager()->flush();
+        /*
+            {
+                "title": "mastery level test",
+                "level_index": 2
+            }
+        */
+        
+        $masteryLevel = $masteryLevelRepository->find($id);
+        if (!$masteryLevel) {
+            
+            return new JsonResponse(['error' => '404 not found.'], 404);
+        }
+        
+        // start of payload validation
+        $keyList = ["title", "level_index"];
 
-            return $this->redirectToRoute('mastery_level_index');
+        $validationsErrors = [];
+
+        $jsonContent = $request->getContent();
+        if (json_decode($jsonContent) === null) {
+            return $this->json([
+                'error' => 'Format de données érroné.'
+            ], Response::HTTP_UNPROCESSABLE_ENTITY);
         }
 
-        return $this->render('mastery_level/edit.html.twig', [
-            'mastery_level' => $masteryLevel,
-            'form' => $form->createView(),
-        ]);
+        // get payload content and convert it to object, so we can acess it's properties
+        $contentObject = json_decode($request->getContent());
+        $contentArray = get_object_vars($contentObject);
+
+        foreach($keyList as $key){
+            if(!array_key_exists($key, $contentArray)){
+                $validationsErrors[] = [
+                                        $key => "Requiered, but not provided"
+                                        ];
+            }
+        }
+
+        if (count($validationsErrors) !== 0) {
+            return $this->json($validationsErrors, Response::HTTP_UNPROCESSABLE_ENTITY);
+        }
+        // end of payload validation
+
+
+
+        // values validation
+
+        $masteryLevelTitle = $contentObject->title;
+        $masteryLevelIndex = $contentObject->level_index;
+        
+        if($masteryLevelIndex === ""){
+            $masteryLevelIndex = 0;
+        }
+
+
+        $validationsErrors = [];
+        
+        if($masteryLevelTitle === ""){
+            $validationsErrors[] = "Title, blank";
+        }
+
+        if(strlen($masteryLevelTitle) > 64){
+            $validationsErrors[] = "title, length, max, 64";
+        }
+
+        if(gettype($masteryLevelIndex) !== "integer"){
+            $validationsErrors[] = "levelIndex, not integer";
+        }
+
+        if($masteryLevelIndex < 0){
+            $validationsErrors[] = "levelIndex, value, min, 0";
+        }
+
+        if($masteryLevelIndex > 99){
+            $validationsErrors[] = "levelIndex, value, max, 99";
+        }
+
+
+        if (count($validationsErrors) !== 0) {
+            return $this->json($validationsErrors, Response::HTTP_UNPROCESSABLE_ENTITY);
+        }
+
+        $masteryLevel->setTitle($masteryLevelTitle);
+        $masteryLevel->setUpdatedAt(new \DateTime());
+        $masteryLevel->setLevelIndex($masteryLevelIndex);
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($masteryLevel);
+        $em->flush();
+        return $this->redirectToRoute('mastery_level_show', ['id' => $masteryLevel->getId()], Response::HTTP_CREATED);
     }
 
     /**
