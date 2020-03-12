059fa9b53beb160e05d5b0caab87ccf31b478e37
unknown <m.ilin.fr@gmail.com>
2020-03-12 20:40:11 +0100

    admin : draft crud

---
+++ src/Controller/Admin/ExerciseCommentController.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\ExerciseComment;
+use App\Form\ExerciseCommentType;
+use App\Repository\ExerciseCommentRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/exercise/comment")
+ */
+class ExerciseCommentController extends AbstractController
+{
+    /**
+     * @Route("/", name="exercise_comment_index", methods={"GET"})
+     */
+    public function index(ExerciseCommentRepository $exerciseCommentRepository): Response
+    {
+        return $this->render('exercise_comment/index.html.twig', [
+            'exercise_comments' => $exerciseCommentRepository->findAll(),
+        ]);
+    }
+
+    /**
+     * @Route("/new", name="exercise_comment_new", methods={"GET","POST"})
+     */
+    public function new(Request $request): Response
+    {
+        $exerciseComment = new ExerciseComment();
+        $form = $this->createForm(ExerciseCommentType::class, $exerciseComment);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($exerciseComment);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('exercise_comment_index');
+        }
+
+        return $this->render('exercise_comment/new.html.twig', [
+            'exercise_comment' => $exerciseComment,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="exercise_comment_show", methods={"GET"})
+     */
+    public function show(ExerciseComment $exerciseComment): Response
+    {
+        return $this->render('exercise_comment/show.html.twig', [
+            'exercise_comment' => $exerciseComment,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}/edit", name="exercise_comment_edit", methods={"GET","POST"})
+     */
+    public function edit(Request $request, ExerciseComment $exerciseComment): Response
+    {
+        $form = $this->createForm(ExerciseCommentType::class, $exerciseComment);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('exercise_comment_index');
+        }
+
+        return $this->render('exercise_comment/edit.html.twig', [
+            'exercise_comment' => $exerciseComment,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="exercise_comment_delete", methods={"DELETE"})
+     */
+    public function delete(Request $request, ExerciseComment $exerciseComment): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$exerciseComment->getId(), $request->request->get('_token'))) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->remove($exerciseComment);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('exercise_comment_index');
+    }
+}
---
+++ src/Controller/Admin/PrerequisiteController.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Prerequisite;
+use App\Form\PrerequisiteType;
+use App\Repository\PrerequisiteRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/prerequisite")
+ */
+class PrerequisiteController extends AbstractController
+{
+    /**
+     * @Route("/", name="prerequisite_index", methods={"GET"})
+     */
+    public function index(PrerequisiteRepository $prerequisiteRepository): Response
+    {
+        return $this->render('prerequisite/index.html.twig', [
+            'prerequisites' => $prerequisiteRepository->findAll(),
+        ]);
+    }
+
+    /**
+     * @Route("/new", name="prerequisite_new", methods={"GET","POST"})
+     */
+    public function new(Request $request): Response
+    {
+        $prerequisite = new Prerequisite();
+        $form = $this->createForm(PrerequisiteType::class, $prerequisite);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($prerequisite);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('prerequisite_index');
+        }
+
+        return $this->render('prerequisite/new.html.twig', [
+            'prerequisite' => $prerequisite,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="prerequisite_show", methods={"GET"})
+     */
+    public function show(Prerequisite $prerequisite): Response
+    {
+        return $this->render('prerequisite/show.html.twig', [
+            'prerequisite' => $prerequisite,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}/edit", name="prerequisite_edit", methods={"GET","POST"})
+     */
+    public function edit(Request $request, Prerequisite $prerequisite): Response
+    {
+        $form = $this->createForm(PrerequisiteType::class, $prerequisite);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('prerequisite_index');
+        }
+
+        return $this->render('prerequisite/edit.html.twig', [
+            'prerequisite' => $prerequisite,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="prerequisite_delete", methods={"DELETE"})
+     */
+    public function delete(Request $request, Prerequisite $prerequisite): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$prerequisite->getId(), $request->request->get('_token'))) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->remove($prerequisite);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('prerequisite_index');
+    }
+}
