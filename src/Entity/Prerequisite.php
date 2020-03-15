482a3ea8d414b4790e9c7684c5a1600adad2cb25
Richard <ric.abitbol@gmail.com>
2020-03-16 00:40:09 +0100

    Fix Prerequisite Route and CreatedAt

--- src/Entity/Prerequisite.php
+++ src/Entity/Prerequisite.php
@@ -2,10 +2,11 @@
 
 namespace App\Entity;
 
-use Doctrine\Common\Collections\ArrayCollection;
-use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\Common\Collections\ArrayCollection;
 use Symfony\Component\Serializer\Annotation\Groups;
+use Symfony\Component\Validator\Constraints\DateTime;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\PrerequisiteRepository")
