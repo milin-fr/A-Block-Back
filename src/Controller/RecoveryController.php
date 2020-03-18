176baa56965cb4ebae026cfdcc996e6978387a7c
unknown <m.ilin.fr@gmail.com>
2020-03-18 16:49:43 +0100

    fixing password recovery controller draft

--- src/Controller/RecoveryController.php
+++ src/Controller/RecoveryController.php
@@ -2,17 +2,21 @@
 
 namespace App\Controller;
 
+use App\Form\UserNewType;
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class RecoveryController extends AbstractController
 {
 /**
      * @Route("/password-recovery", name="password_recovery", methods={"GET","POST"})
      */
-    public function passwordRecovery(Request $request, UserPasswordEncoderInterface $encoder): Response
+    public function passwordRecovery(Request $request, UserPasswordEncoderInterface $encoder, UserRepository $userRepository): Response
     {
-        $user = new User();
         $form = $this->createForm(UserNewType::class, $user);
         $form->handleRequest($request);
 
