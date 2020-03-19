24136ae1b3e6f8ecd12635d51a2e018e7184eb3a
unknown <m.ilin.fr@gmail.com>
2020-03-19 12:04:14 +0100

    additional info on program when requesting user api fixing typo2

--- src/Entity/Program.php
+++ src/Entity/Program.php
@@ -30,19 +30,19 @@ class Program
 
     /**
      * @ORM\Column(type="text", nullable=true)
-     * @Groups{"program", "abloc_user"}
+     * @Groups({"program", "abloc_user"})
      */
     private $description;
 
     /**
      * @ORM\Column(type="smallint")
-     * @Groups{"program", "abloc_user"}
+     * @Groups({"program", "abloc_user"})
      */
     private $time;
 
     /**
      * @ORM\Column(type="string", length=64, nullable=true)
-     * @Groups{"program", "abloc_user"}
+     * @Groups({"program", "abloc_user"})
      */
     private $img_path;
 
