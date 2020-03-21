50f0583f01fdcb34dfa319aeb82624c4f5229be4
Richard <ric.abitbol@gmail.com>
2020-03-21 21:52:36 +0100

    MasteryLevel- LevelIndex add on views - +Assert

--- src/Form/MasteryLevelType.php
+++ src/Form/MasteryLevelType.php
@@ -6,13 +6,17 @@ use App\Entity\MasteryLevel;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 
 class MasteryLevelType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('title')
+            ->add('title', TextType::class)
+            ->add('level_index', IntegerType::class, [
+                'label' => 'Level Index (0 easier than 1)'])
         ;
     }
 
